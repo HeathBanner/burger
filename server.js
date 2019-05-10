8e3e6a0f4d0c77c3a19ba260ebdeb0198fa574a6
Heath Banner <God@Heaths-MacBook-Pro.local>
2019-05-10 10:08:18 -0400

    test

--- server.js
+++ server.js
@@ -11,10 +11,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.engine('handlebars', exphbs({ 
-    defaultLayout: "main", 
-    layoutsDir: path.join(__dirname, 'views')
-}));
+app.engine('handlebars', exphbs({ defaultLayout: "main" }));
 app.set('view engine', 'handlebars');
 
 app.use(routes);
